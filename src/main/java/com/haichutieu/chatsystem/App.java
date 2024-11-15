d9fb9513fd32c0476d54cfdde1a62b894ac7b2b8
phuochoan <89370644+PhuocHoan@users.noreply.github.com>
2024-11-15 09:47:49 +0700

    feat: first draft login UI, no certainty

--- src/main/java/com/haichutieu/chatsystem/App.java
+++ src/main/java/com/haichutieu/chatsystem/App.java
@@ -1,5 +1,6 @@
 package com.haichutieu.chatsystem;
 
+import com.haichutieu.chatsystem.client.login.Login;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,8 +11,9 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(App.class.getResource("style.css").toExternalForm()); // apply style.css to javafx
         stage.setTitle("Chat System");
         stage.setScene(scene);
         stage.setMaximized(true);
--- src/main/java/com/haichutieu/chatsystem/client/login/Login.java
+++ src/main/java/com/haichutieu/chatsystem/client/login/Login.java
@@ -1,4 +1,5 @@
 package com.haichutieu.chatsystem.client.login;
 
 public class Login {
+
 }
