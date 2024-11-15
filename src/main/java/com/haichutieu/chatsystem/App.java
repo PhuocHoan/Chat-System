e6a268001ab0cd75bf53d180fd96214b09885d5b
phuochoan <89370644+PhuocHoan@users.noreply.github.com>
2024-11-15 21:19:33 +0700

    feat: basically finish login and register page -   Bug: when the page is set to maximize size, the screen is collapsed to left side. -   Feature: change scene from login to register page and vice versa.

--- src/main/java/com/haichutieu/chatsystem/App.java
+++ src/main/java/com/haichutieu/chatsystem/App.java
@@ -1,28 +1,29 @@
 package com.haichutieu.chatsystem;
 
-import com.haichutieu.chatsystem.client.login.Login;
+import com.haichutieu.chatsystem.client.Login;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class App extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(App.class.getResource("style.css").toExternalForm()); // apply style.css to javafx
-        stage.setTitle("Chat System");
-        stage.setScene(scene);
-        stage.setMinHeight(800);
-        stage.setMinWidth(900);
-        stage.setMaximized(true);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("client/login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            scene.getStylesheets().add(App.class.getResource("style.css").toExternalForm()); // apply style.css to javafx
+            stage.setTitle("Chat System");
+            stage.setScene(scene);
+            stage.setMinHeight(700);
+            stage.setMinWidth(900);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
